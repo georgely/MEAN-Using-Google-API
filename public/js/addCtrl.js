c60124f37f0dc7b5abaeece825ff15e6b6935058
yue liu <liuyuexj@gmail.com>
2016-07-10 22:01:17 -0700

    Practice MEAN stack with using Google API

---
+++ public/js/addCtrl.js
@@ -0,0 +1,49 @@
+var addCtrl = angular.module('addCtrl', ['geolocation', 'gservice']);
+
+addCtrl.controller('addCtrl', function ($scope, $http, $rootScope, geolocation, gservice) {
+
+    $scope.formData = {};
+    var coords = {};
+    var lat = 0;
+    var long = 0;
+
+    $scope.formData.latitude = 39.500;
+    $scope.formData.longitude = -98.350;
+
+    // Get coordinates based on mouse click. When a click event is detected....
+    $rootScope.$on("clicked", function(){
+
+        // Run the gservice functions associated with identifying coordinates
+        $scope.$apply(function(){
+            $scope.formData.latitude = parseFloat(gservice.clickLat).toFixed(3);
+            $scope.formData.longitude = parseFloat(gservice.clickLong).toFixed(3);
+            $scope.formData.htmlverified = "Nope (Thanks for spamming my map...)";
+        });
+    });
+
+    $scope.createUser = function () {
+        var userData = {
+            username: $scope.formData.username,
+            gender: $scope.formData.gender,
+            age: $scope.formData.age,
+            favlang: $scope.formData.favlang,
+            location: [$scope.formData.longitude, $scope.formData.latitude],
+            htmlverified: $scope.formData.htmlverified
+        };
+        
+        //save the user data to the db
+        $http.post('/users', userData)
+            .success(function (data) {
+                // Once complete, clear the form (except location)
+                $scope.formData.username = "";
+                $scope.formData.gender = "";
+                $scope.formData.age = "";
+                $scope.formData.favlang = "";
+                // Refresh the map with new data
+                gservice.refresh($scope.formData.latitude, $scope.formData.longitude);
+            })
+            .error(function (data) {
+               console.log('Error: ' + data);
+            });
+    };
+});
